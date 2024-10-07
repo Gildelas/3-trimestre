4eb21a8fc79251b406aa1af8e83aba3c9e8155c7
Gildelas <160533362+Gildelas@users.noreply.github.com>
2024-10-07 15:59:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Assassins creed',
+    'Qual a principal regra do credo',
+    'Não machucar inosentes a qualquer custo'
+    )
+    
+    criaCartao(
+    'Assassins creed',
+    'Qual o jogo de pirata da franquia?',
+    'Assassins creed Black Flag'
+    )
+    
+    criaCartao(
+    'Red dead Redemption',
+    'Qual o cavalo mais rapido do jogo?',
+    'Puro sangue árabe preto'
+    )
+    
+    criaCartao (
+    'God of War',
+    'Qauis mitologias que kratos ja visitou?',
+    'Mitologia Grega,Egípcia e Nordica'
+    )
